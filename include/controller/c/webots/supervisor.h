c9f70155ccdebc753f1fc48967c901b3e2efdb6d
Peta Carlyle <petacarlyle@bigpond.com>
2021-06-17 16:51:11 +1000

    ASSUMES PYTHON3.9.5 INSTALLED BY BREW
    It builds on this machine damnit

--- include/controller/c/webots/supervisor.h
+++ include/controller/c/webots/supervisor.h
@@ -127,8 +127,8 @@ void wb_supervisor_node_add_force(WbNodeRef node, const double force[3], bool re
 void wb_supervisor_node_add_force_with_offset(WbNodeRef node, const double force[3], const double offset[3], bool relative);
 void wb_supervisor_node_add_torque(WbNodeRef node, const double torque[3], bool relative);
 
-int wb_supervisor_node_get_number_of_fields(WbNodeRef node);
-int wb_supervisor_node_get_proto_number_of_fields(WbNodeRef node);
+const int wb_supervisor_node_get_number_of_fields(WbNodeRef node);
+const int wb_supervisor_node_get_proto_number_of_fields(WbNodeRef node);
 const char *wb_supervisor_field_get_name(WbFieldRef field);
 WbFieldType wb_supervisor_field_get_type(WbFieldRef field);
 const char *wb_supervisor_field_get_type_name(WbFieldRef field);
